5b6dfc55d8ae72e362686a78b0f28295ee95f065
AACHOURMOHAMED <92208712+AACHOURMOHAMED@users.noreply.github.com>
2022-06-27 16:56:23 +0100

    Add books and remove books from local storage and from the page

--- script/app.js
+++ script/app.js
@@ -1,89 +1,97 @@
-const add = document.querySelector('.add');
-const title = document.querySelector('#title');
+const input = document.querySelector('#title');
 const author = document.querySelector('#Author');
-const list = document.querySelector('#list');
+const taskDiv = document.querySelector('#list');
+const Add = document.querySelector('.add');
 
-add.addEventListener('click', () => {
-  const Title = title.value;
-  const Author = author.value;
-  const lists = document.createElement('div');
-  lists.classList.add('lists');
-  list.appendChild(lists);
-  const listsContentTitle = document.createElement('div');
-  listsContentTitle.classList.add('lists-content' , 'Titlee');
-  listsContentTitle.textContent = Title;
-  lists.appendChild(listsContentTitle);
-  const listsContentAuthor = document.createElement('div');
-  listsContentAuthor.classList.add('lists-content', 'Áuthorr');
-  listsContentAuthor.textContent = Author;
-  lists.appendChild(listsContentAuthor);
-  const remove = document.createElement('button');
-  remove.classList.add('remove-btn');
-  remove.textContent = 'remove';
-  remove.type = 'button';
-  lists.appendChild(remove);
-  remove.addEventListener('click', () => {
-    lists.parentNode.removeChild(lists);
-  });
-  const hr = document.createElement('hr');
-  lists.appendChild(hr);
-});
+// Empty Array To store The Tasks
+let arrayOfTasks = [];
 
-const lisTitle = document.querySelector('.Titlee');
-const lisA = document.querySelector('.Áuthorr');
-
-
-function handleChange() {
-  const formData = {
-    bookTitle: title.value, 
-    bookAuthor: author.value
-  };
-  localStorage.setItem('form', JSON.stringify(formData));
+// Check If theres Tasks In Local Storge
+if (localStorage.getItem('tasks')) {
+  arrayOfTasks = JSON.parse(localStorage.getItem('tasks'));
 }
+const addElementToPageFrom = (arrayOfTasks) => {
+  // Empty Taskk Div
+  taskDiv.innerHTML = '';
+  // Looping on Array of Tasks
+  arrayOfTasks.forEach((task) => {
+    // create Main Div
+    const div = document.createElement('div');
+    div.className = 'task';
+    div.setAttribute('data-id', task.id);
+    // Add Title to the main div
+    const title = document.createElement('div');
+    title.className = 'titleTask';
+    title.textContent = task.title;
+    div.appendChild(title);
+    // Add Author to the main div
+    const Author = document.createElement('div');
+    Author.className = 'AuthorTask';
+    Author.textContent = task.author;
+    div.appendChild(Author);
+    // create remove button
+    const span = document.createElement('button');
+    span.className = 'del';
+    span.type = 'button';
+    span.appendChild(document.createTextNode('remove'));
+    // Append Button To main Div
+    div.appendChild(span);
+    // Add task Div To Tasks container
+    taskDiv.appendChild(div);
+  });
+};
 
-document.addEventListener('DOMContentLoaded', () => {
-  // const getFormValue = localStorage.getItem('form');
-  lisTitle.innerHTML = localStorage.getItem('form');
-  if (getFormValue) {
-    const formObject = JSON.parse(getFormValue);
-    lisTitle.textContent = formObject.bookTitle;
-    lisA.textContent = formObject.bookAuthor;
+const AddToStorge = (arrayOfTasks) => {
+  window.localStorage.setItem('tasks', JSON.stringify(arrayOfTasks));
+};
+
+const getData = () => {
+  const data = window.localStorage.getItem('tasks');
+  if (data) {
+    const tasks = JSON.parse(data);
+    addElementToPageFrom(tasks);
+  }
+};
+// use filter method
+const deleteTask = (taskId) => {
+  // eslint-disable-next-line eqeqeq
+  arrayOfTasks = arrayOfTasks.filter((task) => task.id != taskId);
+  AddToStorge(arrayOfTasks);
+};
+
+// click on task Element
+taskDiv.addEventListener('click', (e) => {
+  // remove Element from page
+  if (e.target.classList.contains('del')) {
+    // remove task from Local storage
+    deleteTask(e.target.parentElement.getAttribute('data-id'));
+    // Remove Element from page
+    e.target.parentElement.remove();
   }
 });
 
-title.onchange = handleChange;
-author.onchange = handleChange;
+// Trigger Get Data From Local Stroge Function
+getData();
+const addTaskToArray = (taskTitle, taskAuthor) => {
+  // Task Data
+  const task = {
+    id: Date.now(),
+    title: taskTitle,
+    author: taskAuthor,
+  };
+  // Push Task to Array of Tasks
+  arrayOfTasks.push(task);
+  // Add Tasks To page
+  addElementToPageFrom(arrayOfTasks);
+  // Add Tasks to  Local Storage
+  AddToStorge(arrayOfTasks);
+};
 
-
-
-
-// const saveBookData = (title, author) => {
-//   const bookData = {
-//     bookTitle: title, 
-//     bookAuthor: author
-//   };
-//   localStorage.setItem('bookData', JSON.stringify(bookData));
-// };
-// console.log(title.value);
-// const getBookData = () => {
-//   const bookData = JSON.parse(localStorage.getItem('bookData'));
-//   if (bookData) {
-//     title.value = bookData.bookTitle;
-//     author.value = bookData.bookAuthor;
-//   };
-// };
-// getBookData;
-
-// saveBookData(title.value, author.value);
-
-// const getFormData = () => {
-//   const formData = JSON.parse(localStorage.getItem('formData'));
-//   if (formData) {
-//     clientName.value = formData.userName;
-//     emailInfo.value = formData.userEmail;
-//     clientMessage.value = formData.userMessage;
-//   }
-// };
-// getFormData();
-
-
+// Add task
+Add.onclick = () => {
+  if (input.value !== '' && author.value !== '') {
+    addTaskToArray(input.value, author.value); // Add task To Array of Tasks
+    author.value = '';
+    input.value = ''; // Empty Input Feild
+  }
+};
