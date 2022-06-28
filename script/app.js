815fc7fe7a0555398916442b2f14975c5336b50f
AACHOURMOHAMED <92208712+AACHOURMOHAMED@users.noreply.github.com>
2022-06-28 13:20:59 +0100

    Add classes to javascript code

--- script/app.js
+++ script/app.js
@@ -3,95 +3,114 @@ const author = document.querySelector('#Author');
 const taskDiv = document.querySelector('#list');
 const Add = document.querySelector('.add');
 
-// Empty Array To store The Tasks
-let arrayOfTasks = [];
-
-// Check If theres Tasks In Local Storge
-if (localStorage.getItem('tasks')) {
-  arrayOfTasks = JSON.parse(localStorage.getItem('tasks'));
-}
-const addElementToPageFrom = (arrayOfTasks) => {
-  // Empty Taskk Div
-  taskDiv.innerHTML = '';
-  // Looping on Array of Tasks
-  arrayOfTasks.forEach((task) => {
-    // create Main Div
-    const div = document.createElement('div');
-    div.className = 'task';
-    div.setAttribute('data-id', task.id);
-    // Add Title to the main div
-    const title = document.createElement('div');
-    title.className = 'titleTask';
-    title.textContent = task.title;
-    div.appendChild(title);
-    // Add Author to the main div
-    const Author = document.createElement('div');
-    Author.className = 'AuthorTask';
-    Author.textContent = task.author;
-    div.appendChild(Author);
-    // create remove button
-    const span = document.createElement('button');
-    span.className = 'del';
-    span.type = 'button';
-    span.appendChild(document.createTextNode('remove'));
-    // Append Button To main Div
-    div.appendChild(span);
-    // Add task Div To Tasks container
-    taskDiv.appendChild(div);
-  });
-};
-
-const AddToStorge = (arrayOfTasks) => {
-  window.localStorage.setItem('tasks', JSON.stringify(arrayOfTasks));
-};
-
-const getData = () => {
-  const data = window.localStorage.getItem('tasks');
-  if (data) {
-    const tasks = JSON.parse(data);
-    addElementToPageFrom(tasks);
+class AwesomeBooks {
+  constructor() {
+    // Empty Array To store The Tasks
+    this.arrayOfTasks = [];
   }
-};
-// use filter method
-const deleteTask = (taskId) => {
-  // eslint-disable-next-line eqeqeq
-  arrayOfTasks = arrayOfTasks.filter((task) => task.id != taskId);
-  AddToStorge(arrayOfTasks);
-};
 
-// click on task Element
-taskDiv.addEventListener('click', (e) => {
-  // remove Element from page
-  if (e.target.classList.contains('del')) {
-    // remove task from Local storage
-    deleteTask(e.target.parentElement.getAttribute('data-id'));
-    // Remove Element from page
-    e.target.parentElement.remove();
+  // Check If theres Tasks In Local Storge
+  checkLocal() {
+    if (localStorage.getItem('tasks')) {
+      this.arrayOfTasks = JSON.parse(localStorage.getItem('tasks'));
+    }
   }
-});
 
-// Trigger Get Data From Local Stroge Function
-getData();
-const addTaskToArray = (taskTitle, taskAuthor) => {
-  // Task Data
-  const task = {
-    id: Date.now(),
-    title: taskTitle,
-    author: taskAuthor,
+  addElementToPageFrom = (arrayOfTasks) => {
+    // Empty Taskk Div
+    taskDiv.innerHTML = '';
+    // Looping on Array of Tasks
+    arrayOfTasks.forEach((task) => {
+      // create Main Div
+      const div = document.createElement('tr');
+      div.className = 'task';
+      div.setAttribute('data-id', task.id);
+      // Add Title to the main div
+      const title = document.createElement('td');
+      title.className = 'titleTask';
+      title.textContent = `"${task.title}" by`;
+      title.style.marginRight = '7px';
+      div.appendChild(title);
+      // Add Author to the main div
+      const Author = document.createElement('td');
+      Author.className = 'AuthorTask';
+      Author.textContent = task.author;
+      div.appendChild(Author);
+      // create remove button
+      const span = document.createElement('button');
+      span.className = 'del';
+      span.type = 'button';
+      span.appendChild(document.createTextNode('remove'));
+      // Append Button To main Div
+      div.appendChild(span);
+      // Add task Div To Tasks container
+      taskDiv.appendChild(div);
+    });
   };
-  // Push Task to Array of Tasks
-  arrayOfTasks.push(task);
-  // Add Tasks To page
-  addElementToPageFrom(arrayOfTasks);
-  // Add Tasks to  Local Storage
-  AddToStorge(arrayOfTasks);
-};
 
-// Add task
-Add.onclick = () => {
-  if (input.value !== '' && author.value !== '') {
-    addTaskToArray(input.value, author.value); // Add task To Array of Tasks
-    author.value = '';
-    input.value = ''; // Empty Input Feild
+  AddToStorge = (arrayOfTasks) => {
+    window.localStorage.setItem('tasks', JSON.stringify(arrayOfTasks));
+  };
+
+  getData = () => {
+    const data = window.localStorage.getItem('tasks');
+    if (data) {
+      const tasks = JSON.parse(data);
+      this.addElementToPageFrom(tasks);
+    }
+  };
+
+  // use filter method
+  deleteTask = (taskId) => {
+    // eslint-disable-next-line eqeqeq
+    this.arrayOfTasks = this.arrayOfTasks.filter((task) => task.id != taskId);
+    this.AddToStorge(this.arrayOfTasks);
+  };
+
+  // click on task Element
+  remover() {
+    taskDiv.addEventListener('click', (e) => {
+      // remove Element from page
+      if (e.target.classList.contains('del')) {
+        // remove task from Local storage
+        this.deleteTask(e.target.parentElement.getAttribute('data-id'));
+        // Remove Element from page
+        e.target.parentElement.remove();
+      }
+    });
   }
-};
+
+  // Trigger Get Data From Local Stroge Function
+  addTaskToArray = (taskTitle, taskAuthor) => {
+    // Task Data
+    const task = {
+      id: Date.now(),
+      title: taskTitle,
+      author: taskAuthor,
+    };
+    // Push Task to Array of Tasks
+    this.arrayOfTasks.push(task);
+    // Add Tasks To page
+    this.addElementToPageFrom(this.arrayOfTasks);
+    // Add Tasks to  Local Storage
+    this.AddToStorge(this.arrayOfTasks);
+  };
+
+  // Add task
+  handlesubmit() {
+    Add.onclick = () => {
+      if (input.value !== '' && author.value !== '') {
+        this.addTaskToArray(input.value, author.value); // Add task To Array of Tasks
+        author.value = '';
+        input.value = ''; // Empty Input Feild
+      }
+    };
+  }
+}
+
+const books = new AwesomeBooks();
+
+books.handlesubmit();
+books.getData();
+books.checkLocal();
+books.remover();
